06932a347d801c3cec8d7cd80db4cf290bc04f02
Salman Fadal <salmanfadal12@gmail.com>
2026-01-16 16:59:31 -0500

    About us page started

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     print(main_test)
     return render_template('index.html', main=main_test)
 
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
